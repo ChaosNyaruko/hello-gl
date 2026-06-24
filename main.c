470b2b62d6186950c4da453173881deb7c753572
chaosnyaruko <cabbageparadise@gmail.com>
2026-06-24 19:10:23 +0800

    texture exercise3: center pixels

--- main.c
+++ main.c
@@ -22,8 +22,8 @@ void load_texture() {
   glBindTexture(GL_TEXTURE_2D, room);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);	
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
   glGenerateMipmap(GL_TEXTURE_2D);
   stbi_image_free(data);
@@ -36,10 +36,10 @@ void load_texture() {
   glGenTextures(1, &face);
   glActiveTexture(GL_TEXTURE1);
   glBindTexture(GL_TEXTURE_2D, face);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);	
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
   glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
   glGenerateMipmap(GL_TEXTURE_2D);
   stbi_image_free(data);
@@ -161,13 +161,13 @@ int main() {
 
   float vertices[] = {
       // position         // color           // texture coords
-      -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  0.0f, 0.0f,
+      -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  0.45f, 0.45f,
 
-      +0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  2.0f, 0.0f,
+      +0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  0.55f, 0.45f,
 
-      +0.5f, +0.5f, 0.0f, 0.0f, 0.0f, 1.0f,  2.0f, 2.0f, 
+      +0.5f, +0.5f, 0.0f, 0.0f, 0.0f, 1.0f,  0.55f, 0.55f, 
 
-      -0.5f, +0.5f, 0.0f, 1.0f, 1.0f, 0.0f,  0.0f, 2.0f,
+      -0.5f, +0.5f, 0.0f, 1.0f, 1.0f, 0.0f,  0.45f, 0.55f,
   };
 
   unsigned int indices[] = {
